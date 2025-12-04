f1df9cbb7d998a59a0afe72e0bba892818305d06
HaiLH <hai.le@datx.com.vn>
2025-12-05 02:45:02 +0700

    Add init user

---
+++ src/main/java/com/aquariux/cryptotrading/config/DataInitializer.java
@@ -0,0 +1,41 @@
+package com.aquariux.cryptotrading.config;
+
+import com.aquariux.cryptotrading.entity.User;
+import com.aquariux.cryptotrading.entity.Wallet;
+import com.aquariux.cryptotrading.repository.UserRepository;
+import com.aquariux.cryptotrading.repository.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class DataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final WalletRepository walletRepository;
+
+    @Override
+    public void run(String... args) {
+        log.info("Initializing default user and wallet...");
+
+        User user = new User();
+        user.setUsername("default_user");
+        user.setCreatedAt(LocalDateTime.now());
+        user = userRepository.save(user);
+
+        Wallet usdtWallet = new Wallet();
+        usdtWallet.setUser(user);
+        usdtWallet.setCurrency("USDT");
+        usdtWallet.setBalance(new BigDecimal("50000.00000000"));
+        usdtWallet.setUpdatedAt(LocalDateTime.now());
+        walletRepository.save(usdtWallet);
+
+        log.info("Default user created with ID: {} and initial USDT balance: 50000", user.getId());
+    }
+}
